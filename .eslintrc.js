47df4970d331c3b9f05f3800ef4485b25039b42b
Jon Abaunza <jabaunza@sisteplant.com>
2019-04-15 23:39:34 +0200

    Upgraded quasar and corrected minor lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'plugin:jest/recommended'
   ],
 
   // required to lint *.vue files
--- src/pages/__tests__/Index.spec.js
+++ src/pages/__tests__/Index.spec.js
@@ -2,45 +2,44 @@
  * @jest-environment jsdom
  */
 import IndexPage from '../index'
-import { mountPageComponent } from 'test-common';
+import { mountPageComponent } from 'test-common'
 import { LocalStorage } from 'quasar'
 
 /**
  * Contains the tests of the page DashboardDesignerPage
  */
 describe('Dashboard designer page tests', () => {
-
-  let component;
-  let qLocalStorage;
+  let component
+  let qLocalStorage
   let localStorageKey = 'definition'
 
   /**
    * Before each test we recreate the component loading the full tree
    */
   beforeEach(() => {
-  //beforeAll(() => {
+  // beforeAll(() => {
 
-    let quasarPluginInstantiator ={ $q:{} }
+    let quasarPluginInstantiator = { $q: {} }
     LocalStorage.install(quasarPluginInstantiator)
     qLocalStorage = quasarPluginInstantiator.$q.localStorage
     // We create a basic dummy configuration
     let definition = [
-     'Hello',
-     ' ',
-     'World!'
+      'Hello',
+      ' ',
+      'World!'
     ]
     // We use the quasar localStorage plugin to store the configuration
     // as it has a wrapper that allows serialization with types
     qLocalStorage.set(localStorageKey, definition)
 
-    component = mountPageComponent(IndexPage,{}, true)
+    component = mountPageComponent(IndexPage, {}, true)
   })
 
   /**
    * After each test we will cleanup the shared state (localStorage)
    */
   afterEach(() => {
-  //afterAll(() => {
+  // afterAll(() => {
     qLocalStorage.remove(localStorageKey)
   })
 
@@ -51,7 +50,6 @@ describe('Dashboard designer page tests', () => {
   it('passes the sanity check to see if at least is a Vue instance', () => {
     expect(component.isVueInstance()).toBe(true)
   })
-
 
   /**
    * Tests if it is Vue instance.
